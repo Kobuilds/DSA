cddb5477b5d9ee387aed820832c334571b871782
Komalpreet Kaur <150986876+Kobuilds@users.noreply.github.com>
2026-04-01 23:43:36 +0530

    Time: 4 ms (53.07%) | Memory: 14.9 MB (36.15%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp; // value -> index
+   
+        vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp; // value -> index
+    
+    for (int i = 0; i < nums.size(); i++) {
+        int needed = target - nums[i];
         
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            
-            if (mp.find(complement) != mp.end()) {
-                // Found the pair
-                return {mp[complement], i};
-            }
-            
-            mp[nums[i]] = i; // store current number with its index
+        if (mp.find(needed) != mp.end()) {
+            return {mp[needed], i};
         }
         
-        return {}; // no solution found (though guaranteed in problem statement)
+        mp[nums[i]] = i;
     }
+    
+    return {};
+}
+        
+        
+    
 };
